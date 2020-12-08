c43b9c7cc20dff5627b80e0607b0403ec3a9f758
Stanislav Krausov <krausov@bk.ru>
2020-12-08 21:36:32 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 5000;
+        int mile = 1;
+        int spent = 20;
+
+        int bonus = ticket / spent * mile;
+        System.out.println(bonus);
+    }
+}
